329b20ce017c9e0e213d096f2af3a4b4f9b50a27
fabijanski <mm.fabijanski@gmail.com>
2017-08-02 13:48:10 +0200

    fabian day3 - commit 4

---
+++ day3/fabian/level-3.js
@@ -0,0 +1,137 @@
+//17.Flatten the movieLists array into an array of video ids
+//Let's start by using two nested forEach loops to collect the id of every video in the two-dimensional movieLists array.
+
+function task17() {
+    var movieLists = [
+            {
+                name: "New Releases",
+                videos: [
+                    {
+                        "id": 70111470,
+                        "title": "Die Hard",
+                        "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+                        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                        "rating": 4.0,
+                        "bookmark": []
+                    },
+                    {
+                        "id": 654356453,
+                        "title": "Bad Boys",
+                        "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+                        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                        "rating": 5.0,
+                        "bookmark": [{ id: 432534, time: 65876586 }]
+                    }
+                ]
+            },
+            {
+                name: "Dramas",
+                videos: [
+                    {
+                        "id": 65432445,
+                        "title": "The Chamber",
+                        "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+                        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                        "rating": 4.0,
+                        "bookmark": []
+                    },
+                    {
+                        "id": 675465,
+                        "title": "Fracture",
+                        "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+                        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                        "rating": 5.0,
+                        "bookmark": [{ id: 432534, time: 65876586 }]
+                    }
+                ]
+            }
+        ],
+        allVideoIdsInMovieLists = [];
+
+    // put code here
+    movieLists.forEach(function(movie) {
+      movie.videos.forEach(function(video){
+        allVideoIdsInMovieLists.push(`${video.id}`);
+      });
+    });
+
+    // return allVideoIdsInMovieLists;
+    console.log(allVideoIdsInMovieLists);
+
+}
+task17();
+
+//18: Implement concatAll()
+//Let's add a concatAll() function to the Array type. The concatAll() function iterates over each sub-array in the array and collects the results in a new, flat array. Notice that the concatAll() function expects each item in the array to be another array.
+//TIP: this function should work like your solution in task 16
+
+// put code here
+Array.prototype.concatAll = function() {
+  var results = [];
+
+  this.forEach(function(itemInArray) {
+    Object.values(itemInArray).forEach(function(val) {
+      var what = Object.prototype.toString.call(val);
+      results.push(val);
+    });
+  });
+  return results;
+};
+
+//19: Use map() and concatAll() to project and flatten the movieLists into an array of video ids
+//Hint: use two nested calls to map() and one call to concatAll().
+
+function task19() {
+    var movieLists = [
+        {
+            name: "New Releases",
+            videos: [
+                {
+                    "id": 70111470,
+                    "title": "Die Hard",
+                    "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "rating": 4.0,
+                    "bookmark": []
+                },
+                {
+                    "id": 654356453,
+                    "title": "Bad Boys",
+                    "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "rating": 5.0,
+                    "bookmark": [{ id: 432534, time: 65876586 }]
+                }
+            ]
+        },
+        {
+            name: "Dramas",
+            videos: [
+                {
+                    "id": 65432445,
+                    "title": "The Chamber",
+                    "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "rating": 4.0,
+                    "bookmark": []
+                },
+                {
+                    "id": 675465,
+                    "title": "Fracture",
+                    "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "rating": 5.0,
+                    "bookmark": [{ id: 432534, time: 65876586 }]
+                }
+            ]
+        }
+    ];
+
+    // return movieLists. //put code here
+    var mapAndConcatAll = movieLists
+                            .map(movie => movie.videos.map(element => element.id))
+                            .concatAll();
+    console.log(mapAndConcatAll);
+}
+
+task19();
