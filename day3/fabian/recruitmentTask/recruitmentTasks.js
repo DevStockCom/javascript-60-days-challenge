bbc8ba976d40830a66ffe771636e8d447bd43c70
fabijanski <mm.fabijanski@gmail.com>
2017-08-02 22:59:35 +0200

    fabian day3 - commit 5

--- day3/fabian/recruitmentTask/recruitmentTasks.js
+++ day3/fabian/recruitmentTask/recruitmentTasks.js
@@ -22,53 +22,79 @@ Array.prototype.concatAll = function() {
 
   this.forEach(function(itemInArray) {
     Object.values(itemInArray).forEach(function(val) {
-      var what = Object.prototype.toString.call(val);
+      console.log(itemInArray);
       results.push(val);
     });
   });
   return results;
 };
 
+Array.prototype.customFilter = function(predicateFunction) {
+  var results = [];
 
-function recruitmentTask() {
+  this.forEach(function(itemInArray) {
+    console.log(itemInArray);
+    if (predicateFunction.call(this, itemInArray)) {
+      results.push(itemInArray);
+    }
+  });
+  return results;
+};
+
+
+function finalTask() {
     var movieLists = [
         {
-            name: "New Releases",
-            videos: [
+            name: "Instant Queue",
+            videos : [
                 {
                     "id": 70111470,
                     "title": "Die Hard",
-                    "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
-                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "boxarts": [
+                        { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+                        { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" }
+                    ],
+                    "url": "http://api.netflix.com/catalog/titles/movies/70111470",
                     "rating": 4.0,
                     "bookmark": []
                 },
                 {
                     "id": 654356453,
                     "title": "Bad Boys",
-                    "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
-                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "boxarts": [
+                        { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+                        { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/BadBoys150.jpg" }
+
+                    ],
+                    "url": "http://api.netflix.com/catalog/titles/movies/70111470",
                     "rating": 5.0,
                     "bookmark": [{ id: 432534, time: 65876586 }]
                 }
             ]
         },
         {
-            name: "Dramas",
+            name: "New Releases",
             videos: [
                 {
                     "id": 65432445,
                     "title": "The Chamber",
-                    "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
-                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "boxarts": [
+                        { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber150.jpg" },
+                        { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
+                    ],
+                    "url": "http://api.netflix.com/catalog/titles/movies/70111470",
                     "rating": 4.0,
                     "bookmark": []
                 },
                 {
                     "id": 675465,
                     "title": "Fracture",
-                    "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
-                    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+                    "boxarts": [
+                        { width: 200, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+                        { width: 150, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+                        { width: 300, height: 200, url: "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+                    ],
+                    "url": "http://api.netflix.com/catalog/titles/movies/70111470",
                     "rating": 5.0,
                     "bookmark": [{ id: 432534, time: 65876586 }]
                 }
@@ -76,11 +102,14 @@ function recruitmentTask() {
         }
     ];
 
-    // return movieLists. //put code here
-    var mapAndConcatAll = movieLists
-                            .map(movie => movie.videos.map(element => `${element.id}, ${element.title}, ${element.bookmark.filter(x => x.id)}`))
-                            .concatAll();
-    console.log(mapAndConcatAll);
+    // return movieLists. // put code here
+    var getMovieData = movieLists
+      .map(movie => movie.videos
+        .map(element => element.id + ', ' + element.title + ', ' + element.boxarts
+          .filter(boxart => (boxart.width == 150 && boxart.height == 200))
+            .map(x => x.url)))
+      .concatAll()
+    console.log(getMovieData);
 }
 
-recruitmentTask();
+finalTask();
